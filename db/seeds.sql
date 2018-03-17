60dc1f806cc343327074025b98a15d372fdda90e
David Coulombe <David.Coulombe7@gmail.com>
2018-03-16 22:18:48 -0400

    pushing the works

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Western Burger, '
+false');
+INSERT INTO burger (burger_name, devoured) VALUES ('Philly Burger, 'false');
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Best Burger, '
+false');
+INSERT INTO burger (burger_name, devoured) VALUES ('Original Burger, 'false');
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Mexican Burger, '
+false');
