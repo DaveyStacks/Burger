575eca7d0387b7a116d63d8e1e0f8c828ff62638
David Coulombe <David.Coulombe7@gmail.com>
2018-03-18 11:43:46 -0400

    db changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,19 @@
-INSERT INTO burger
+INSERT INTO burgers
     (burger_name, devoured)
 VALUES
-    ('Western Burger, '
-false');
-INSERT INTO burger (burger_name, devoured) VALUES ('Philly Burger, 'false');
-INSERT INTO burger
+    ('Western Burger', false);
+INSERT INTO burgers
     (burger_name, devoured)
 VALUES
-    ('Best Burger, '
-false');
-INSERT INTO burger (burger_name, devoured) VALUES ('Original Burger, 'false');
-INSERT INTO burger
+    ('Philly Burger', false);
+INSERT INTO burgers
     (burger_name, devoured)
 VALUES
-    ('Mexican Burger, '
-false');
+    ('Best Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Original Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('Mexican Burger', false);
